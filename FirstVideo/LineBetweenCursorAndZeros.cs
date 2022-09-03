a7a0de132f7a6646ce870e99e281c59210459432
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2022-09-04 02:22:55 +0300

    Delete FirstVideo directory

--- FirstVideo/LineBetweenCursorAndZeros.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public sealed class LineBetweenCursorAndZeros : MonoBehaviour
-{
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(Vector2.zero, this.transform.position);
-    }
-}
